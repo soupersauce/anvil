3103bef9dd1a2793944e9bd6aabb62099ace3cac
soupersauce <soupersauce@gmail.com>
2022-06-27 17:50:32 -0700

    dap-install no longer exists look at dap-buddy again later

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,15 +4,15 @@ if not status_ok then
 end
 
 -- dap-install configurations
-local dap_install = require("dap-install")
-dap_install.setup({
-  installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
-})
-local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
+-- local dap_install = require("dap-install")
+-- dap_install.setup({
+--   installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
+-- })
+-- local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
 
-for _, debugger in ipairs(dbg_list) do
-  dap_install.config(debugger)
-end
+-- for _, debugger in ipairs(dbg_list) do
+--   dap_install.config(debugger)
+-- end
 
 dap.defaults.fallback.terminal_win_cmd = "ToggleTerm"
 vim.fn.sign_define("DapBreakpoint", { text = "● ", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
