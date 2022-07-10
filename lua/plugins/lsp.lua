c6de10fbcdefb5cd8b197048457aec6c1ae9499d
soupersauce <soupersauce@gmail.com>
2022-07-09 21:35:41 -0700

    Re-enable format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -475,21 +475,21 @@ null_ls.setup {
 	-- debug = true,
 	sources = null_sources,
 	on_attach = function(client, bufnr)
-		-- if client.supports_method('textDocument/formatting') then
-		-- 	vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-		-- 	vim.api.nvim_create_autocmd('BufWritePre', {
-		-- 		group = augroup,
-		-- 		buffer = bufnr,
-		-- 		callback = function()
-		-- 			vim.lsp.buf.format {
-		-- 				bufnr = bufnr,
-		-- 				filter = function(client)
-		-- 					return client.name == 'null-ls'
-		-- 				end,
-		-- 			}
-		-- 		end,
-		-- 	})
-		-- end
+		if client.supports_method('textDocument/formatting') then
+			vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format {
+						bufnr = bufnr,
+						filter = function(client)
+							return client.name == 'null-ls'
+						end,
+					}
+				end,
+			})
+		end
 	end,
 }
 require('docs-view').setup {}
