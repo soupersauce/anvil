a9ab874a58c3875abd5b9b9ca2eef1140404ac82
soupersauce <soupersauce@gmail.com>
2022-07-31 12:02:33 -0700

    tweak blankline

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -4,6 +4,10 @@ if not present then
 	return
 end
 
+vim.opt.list = true
+vim.opt.listchars:append('space:⋅')
+vim.opt.listchars:append('eol:↴')
+
 local options = {
 	indentLine_enabled = 1,
 	char = '▏',
@@ -23,6 +27,7 @@ local options = {
 		'',
 	},
 	buftype_exclude = { 'terminal' },
+	space_char_blankeline = ' ',
 	show_trailing_blankline_indent = false,
 	show_first_indent_level = false,
 	show_current_context = true,
