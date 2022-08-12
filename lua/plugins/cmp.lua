ba7bd58722bb63a4bb78bf214c7cddcd08a5bd2a
soupersauce <soupersauce@gmail.com>
2022-08-11 17:38:25 -0700

    add space

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -50,7 +50,8 @@ local box_icons =
 		Event = '',
 		Operator = '',
 		TypeParameter = '',
-	}, require('luasnip.loaders.from_vscode').lazy_load()
+	},
+require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup {
 	-- cmp.event:on(
