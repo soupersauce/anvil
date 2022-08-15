39a0fc01aa2c125b73f342be4ea4610295e33a0e
soupersauce <soupersauce@gmail.com>
2022-08-15 10:25:57 -0700

    Update UFO to use new mapping

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -7,7 +7,7 @@ end
 
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-vim.keymap.set('n', 'zr', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
 vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
 vim.keymap.set('n', 'K', function()
 	local winid = require('ufo').peekFoldedLinesUnderCursor()
