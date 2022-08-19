65ec0f5b48c34db51a5b6945644d4ae638c1c5ef
soupersauce <soupersauce@gmail.com>
2022-08-19 15:39:27 -0700

    disable vale for now

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -522,10 +522,10 @@ local null_sources = {
 	},
 	null_b.diagnostics.stylelint,
 	null_b.diagnostics.tidy,
-	null_b.diagnostics.vale.with {
-		extra_filetypes = { 'org', 'text' },
-		extra_args = { '--config', vim.fn.expand('~/.vale.ini') },
-	},
+	-- null_b.diagnostics.vale.with {
+	-- 	extra_filetypes = { 'org', 'text' },
+	-- 	extra_args = { '--config', vim.fn.expand('~/.vale.ini') },
+	-- },
 	null_b.diagnostics.yamllint,
 	null_b.diagnostics.zsh,
 
