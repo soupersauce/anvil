daf47bb04413881874dd25a67f52b0dc2d350ab2
soupersauce <soupersauce@gmail.com>
2022-08-27 22:31:55 -0700

    Add git-conflict

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -114,6 +114,21 @@ local on_startup = function(use)
 		},
 	}
 
+	use { -- git-conflict
+		'akinsho/git-conflict.nvim',
+		tag = '*',
+		config = function()
+			require('git-conflict').setup {
+				default_mappings = true, -- disable buffer local mapping created by this plugin
+				disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+				highlights = { -- They must have background color, otherwise the default color will be used
+					incoming = 'DiffText',
+					current = 'DiffAdd',
+				},
+			}
+		end,
+	}
+
 	use { -- TREESITTER: integration
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
