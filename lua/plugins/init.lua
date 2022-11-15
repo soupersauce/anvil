bac80b04cd8d5a3d601780b96c4daf222032dec9
soupersauce <soupersauce@gmail.com>
2022-11-14 17:37:52 -0800

    feat(hlslens & scrollbar)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -501,6 +501,56 @@ local on_startup = function(use)
 		end,
 	}
 
+	use {
+		'kevinhwang91/nvim-hlslens',
+		requires = 'kevinhwang91/nvim-scrollbar',
+		config = function()
+			require('hlslens').setup {
+				build_position_cb = function(plist, _, _, _)
+					require('scrollbar.handlers.search').handler.show(plist.start_pos)
+				end,
+			}
+			local kopts = { noremap = true, silent = true }
+
+			vim.api.nvim_set_keymap(
+				'n',
+				'n',
+				[[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+				kopts
+			)
+			vim.api.nvim_set_keymap(
+				'n',
+				'N',
+				[[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+				kopts
+			)
+			vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+			vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+			vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+			vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+			vim.cmd([[
+        augroup scrollbar_search_hide
+            autocmd!
+            autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+        augroup END
+    ]])
+			vim.api.nvim_set_keymap('n', '<Leader>l', ':noh<CR>', kopts)
+		end,
+	}
+	use {
+		'petertriho/nvim-scrollbar',
+		config = function()
+			require('scrollbar').setup {
+				handlers = {
+					diagnostic = true,
+					search = true, -- Requires hlslens to be loaded
+					gitsigns = true, -- Requires gitsigns.nvim
+				},
+			}
+		end,
+	}
+
 	use { -- tint.nvim
 		'levouh/tint.nvim',
 		config = function()
