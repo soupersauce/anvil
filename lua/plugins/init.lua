a50ec95627b6475d93bb1441d48b2f672b040876
soupersauce <soupersauce@gmail.com>
2023-01-23 22:44:22 -0800

    Else

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,10 +35,50 @@ local M = {
 			}
 		end,
 	},
-
+	{
+		'EtiamNullam/deferred-clipboard.nvim',
+		opts = {
+			lazy = true,
+			fallback = 'unnamedplus',
+		},
+	},
+	{
+		'bfredl/nvim-miniyank',
+		keys = {
+			{ 'p', '<Plug>(miniyank-autoput)', desc = 'Miniyank put' },
+			{ 'P', '<Plug>(miniyank-autoPut)', desc = 'Miniyank Put' },
+			{ '<C-j>', '<Plug>(miniyank-cycle)', desc = 'Miniyank cycle' },
+			{ '<C-k>', '<Plug>(miniyank-cycleback)', desc = 'Miniyank cycleback' },
+		},
+	},
+	{
+		'famiu/bufdelete.nvim',
+		keys = {
+			{
+				'bd',
+				function()
+					require('bufdelete').bufdelete(0, true)
+				end,
+				desc = 'Delete Current buffer',
+			},
+		},
+	},
+	{
+		'stevearc/oil.nvim',
+		config = true,
+	},
 	-- Syntaxes
 	'sqwishy/vim-sqf-syntax', -- Sqf for arma3
-	'tridactyl/vim-tridactyl', -- for tridactyl firefox plugin
+	{
+		'tridactyl/vim-tridactyl', -- for tridactyl firefox plugin
+		event = 'bufReadPre tridactylrc',
+		config = function()
+			vim.api.nvim_create_autocmd({ 'bufRead' }, {
+				pattern = 'tridactylrc',
+				command = 'set ft=tridactylrc',
+			})
+		end,
+	},
 }
 
 return M
