b03055d34382f3507b87bd5ff2a28bc5425111d8
soupersauce <soupersauce@gmail.com>
2023-09-24 00:32:34 -0700

    Add sentiment

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,6 +29,17 @@ local M = {
 		end,
 	},
 	{
+		'utilyre/sentiment.nvim',
+		version = '*',
+		event = 'VeryLazy', -- keep for lazy loading
+		opts = {
+			-- config
+		},
+		init = function()
+			-- `matchparen.vim` needs to be disabled manually in case of lazy loading
+			vim.g.loaded_matchparen = 1
+		end,
+	},
 	{
 		'willothy/moveline.nvim',
 		build = 'make',
